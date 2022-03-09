343cb13df46705ce0abf2cf46a1e5ad286b31ea8
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-09 20:09:35 +0530

    linked js and got the relevant elements

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const form = document.getElementsByTagName("form")[0];
+
+const email = document.getElementById("email");
+const emailError = DocumentFragment.querySelector("#email + span.error");
+const emailErrorIcon = Document.querySelector("form > svg.error-icon");
